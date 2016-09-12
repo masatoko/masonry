e394e281810e86eaa9673c90975c068924593894
masatoko <masatokodo@gmail.com>
2016-09-12 17:34:45 +0900

    RandNum - normRandom

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import System.Random
 
 main :: IO ()
-main = someFunc
+main = return ()
---
+++ src/RandNum.hs
@@ -0,0 +1,12 @@
+module RandNum where
+
+import System.Random
+
+-- let work = normRandom 10 4 . mkStdGen
+-- print $ map (fst . work) [0..10]
+normRandom :: RandomGen g => Double -> Double -> g -> (Double, g)
+normRandom mu sigma g = (z, g'')
+  where
+    (x, g')  = randomR (0,1) g
+    (y, g'') = randomR (0,1) g'
+    z = sigma * sqrt (-2 * log x) * cos (2 * pi * y) + mu
