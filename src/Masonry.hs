77f8096f9461ce649d1c0ced5012540867de8639
masatoko <masatokodo@gmail.com>
2016-09-12 21:34:18 +0900

    SVG with back-ground

--- src/Masonry.hs
+++ src/Masonry.hs
@@ -2,6 +2,7 @@ module Masonry
 ( test
 ) where
 
+import Linear.Affine
 import Linear.V2
 import System.Random
 
@@ -27,5 +28,11 @@ exportRooms (V2 w h) rs =
   where
     stg    = SVG.Settings w h
     fill   = SVG.Fill "#0000ff" 0.4
-    stroke = SVG.Stroke "#ffffff" 0.1 0.8
-    rs'    = map (\r -> SVG.Object (SVG.SRect r) fill stroke) rs
+    stroke = SVG.Stroke "#ffffff" 0.2 0.6
+    --
+    w' = fromIntegral w
+    h' = fromIntegral h
+    bg  = SVG.Object (SVG.SRect (Rect (P $ V2 (w'/2) (h'/2)) (V2 w' h')))
+                     (SVG.Fill "#000000" 1)
+                     (SVG.Stroke "#ffffff" 0 0)
+    rs' = bg : map (\r -> SVG.Object (SVG.SRect r) fill stroke) rs
