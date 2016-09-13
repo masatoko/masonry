8df9ae7f3d51c7294c4e97825c2b2346804968d1
masatoko <masatokodo@gmail.com>
2016-09-13 09:56:42 +0900

    SDL sample code

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Control.Concurrent (threadDelay)
+import qualified SDL
+import Linear.V2
 
 import Masonry (test)
 
 main :: IO ()
-main =
-  test 111
+main = do
+  -- test 111
+
+  SDL.initializeAll
+
+  win <- SDL.createWindow "Masonry" SDL.defaultWindow {SDL.windowInitialSize = V2 800 800}
+  SDL.showWindow win
+
+  threadDelay 1000000
+
+  SDL.destroyWindow win
+  SDL.quit
