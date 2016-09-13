e2f33c270b12ce1e382bdb7c34177d234e915123
masatoko <masatokodo@gmail.com>
2016-09-13 18:33:59 +0900

    withinRect

--- src/Type.hs
+++ src/Type.hs
@@ -1,6 +1,7 @@
 module Type
 ( Rect (..)
 , fat
+, withinRect
 ) where
 
 import Linear.Affine
@@ -15,3 +16,12 @@ fat a (Rect pos size) = Rect pos' size'
   where
     pos'  = pos - pure a
     size' = size + pure (2 * a)
+
+withinRect :: (Num a, Ord a) => Point V2 a -> Rect a -> Bool
+withinRect (P (V2 x y)) (Rect pos size) =
+  x >= x0 && x <= x1 && y >= y0 && y <= y1
+  where
+    P (V2 x0 y0) = pos
+    V2 w h = size
+    x1 = x0 + w
+    y1 = y0 + h
